8793e76ea45a5856b4eb8cd02544ce94005b6429
Dagger-shailendra <57583120+Dagger-shailendra@users.noreply.github.com>
2020-09-25 00:35:01 +0530

    Add files via upload

---
+++ notifications.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:new_hoe/models/post.dart';
+import 'package:new_hoe/models/user.dart';
+import 'package:new_hoe/models/comment.dart';
+import 'package:new_hoe/models/global.dart';
+
+class NotificationsPage extends StatefulWidget {
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
